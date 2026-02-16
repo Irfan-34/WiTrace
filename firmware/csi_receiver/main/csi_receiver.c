21523dbea3beb0e92f8a5b664ff3e2ea2e8a780d
Mizhab A S <mizhabayyaril@gmail.com>
2026-02-16 21:23:40 +0530

    CSI firmware verified working

--- firmware/csi_receiver/main/csi_receiver.c
+++ firmware/csi_receiver/main/csi_receiver.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_netif.h"
+
+#define WIFI_SSID "Home 2.4G"
+#define WIFI_PASS "SUB9447670933"
 
 static const char *TAG = "CSI";
 
+/* CSI callback function */
 static void wifi_csi_cb(void *ctx, wifi_csi_info_t *info)
 {
     printf("CSI_DATA: ");
@@ -16,15 +22,63 @@ static void wifi_csi_cb(void *ctx, wifi_csi_info_t *info)
     printf("\n");
 }
 
+/* WiFi event handler */
+static void wifi_event_handler(void* arg,
+                               esp_event_base_t event_base,
+                               int32_t event_id,
+                               void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        esp_wifi_connect();
+        ESP_LOGI(TAG, "Retrying WiFi connection...");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ESP_LOGI(TAG, "WiFi connected. CSI should start appearing.");
+    }
+}
+
+/* WiFi initialization with CSI enabled */
 void wifi_init()
 {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
 
+    esp_event_handler_instance_register(WIFI_EVENT,
+                                        ESP_EVENT_ANY_ID,
+                                        &wifi_event_handler,
+                                        NULL,
+                                        NULL);
+
+    esp_event_handler_instance_register(IP_EVENT,
+                                        IP_EVENT_STA_GOT_IP,
+                                        &wifi_event_handler,
+                                        NULL,
+                                        NULL);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
     esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
     esp_wifi_start();
 
-    wifi_csi_config_t config = {
+    /* CSI configuration */
+    wifi_csi_config_t csi_config = {
         .lltf_en = true,
         .htltf_en = true,
         .stbc_htltf2_en = true,
@@ -34,15 +88,17 @@ void wifi_init()
         .shift = false
     };
 
-    esp_wifi_set_csi_config(&config);
+    esp_wifi_set_csi_config(&csi_config);
     esp_wifi_set_csi_rx_cb(&wifi_csi_cb, NULL);
     esp_wifi_set_csi(true);
+
+    ESP_LOGI(TAG, "CSI initialized");
 }
 
+/* Main entry */
 void app_main()
 {
     nvs_flash_init();
-    esp_event_loop_create_default();
     wifi_init();
 }
 
